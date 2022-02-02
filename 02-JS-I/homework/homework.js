1de7f9d4cb84d3d0ab67d6b777ed95cbbc165866
Purple22x <dami.c@live.com.ar>
2022-02-02 20:56:04 -0300

    tarea

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -224,11 +224,11 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   if (letra.length >1) { 
-    return "dato incorrecto"
+    return "Dato incorrecto"
   }
   if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
   return "Es vocal" }
-  return "dato incorrecto"
+  return "Dato incorrecto"
 
 }
 
